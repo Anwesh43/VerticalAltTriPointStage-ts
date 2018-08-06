ca2dfd692366e54f9a3b6e19e9f426f165c3b2a0
Anwesh43 <anweshthecool0@gmail.com>
2018-08-06 11:07:13 +0530

    adding VATPNode class

--- VerticalAltTriPointStage.ts
+++ VerticalAltTriPointStage.ts
@@ -1,4 +1,5 @@
 const w : number = window.innerWidth, h : number = window.innerHeight
+const nodes : number = 5
 class VerticalAltTriPointStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
@@ -72,5 +73,63 @@ class Animator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class VATPNode {
+    prev : VATPNode
+    next : VATPNode
+    state : State = new State()
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new VATPNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const hGap = h / nodes
+        const size = hGap/3
+        const factor = (1 - 2 * (this.i % 2))
+        const sc1 = Math.min(0.5, this.state.scale) * 2
+        const sc2 = Math.min(0.5, Math.max(0, this.state.scale - 0.5)) * 2
+        context.fillStyle = 'white'
+        context.strokeStyle = 'white'
+        context.lineWidth = hGap/15
+        context.lineCap = 'round'
+        context.save()
+        context.translate(w/2 - hGap/2 * factor, hGap * this.i + hGap/2)
+        context.beginPath()
+        context.arc(hGap * factor * sc2, hGap * sc2, size/2, 0, 2 * Math.PI)
+        context.fill()
+        context.beginPath()
+        context.moveTo(hGap * factor * sc2, hGap * sc2)
+        context.lineTo(hGap * factor * sc1, hGap * sc1)
+        context.stroke()
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : VATPNode {
+        var curr : VATPNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
     }
 }
